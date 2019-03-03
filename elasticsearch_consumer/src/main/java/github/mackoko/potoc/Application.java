a461ac51d39a2d0057533a68e108a842c7b334db
mackoko <maciej.rzeszutko@collibra.com>
2019-03-04 00:02:55 +0100

    elastic template

---
+++ elasticsearch_consumer/src/main/java/github/mackoko/potoc/Application.java
@@ -0,0 +1,27 @@
+package github.mackoko.potoc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+
+	private static Logger LOG = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		LOG.info("STARTING THE APPLICATION");
+		SpringApplication.run(Application.class, args);
+		LOG.info("APPLICATION FINISHED");
+	}
+
+	@Override
+	public void run(String... args) {
+		LOG.info("EXECUTING : command line runner");
+
+	}
+}
