78baa3a52f40c5ad827999378c35d2f0f98a3955
mackoko <maciej.rzeszutko@collibra.com>
2019-05-31 13:38:01 +0200

    Evolution check - only record is validated. If enum symbols changed nth. happens. Consumer throws exception if cannot map.

--- avro_kafka_v2/src/main/java/com/github/mackoko/avro/enums/EnumsConsumer.java
+++ avro_kafka_v2/src/main/java/com/github/mackoko/avro/enums/EnumsConsumer.java
@@ -1,23 +1,32 @@
 package com.github.mackoko.avro.enums;
 
 
+import static com.github.mackoko.avro.util.KafkaUtil.TOPIC_ENUMS;
+import static com.github.mackoko.avro.util.KafkaUtil.consumerProperties;
+import static java.util.Collections.singleton;
+
+import java.time.Duration;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
 public class EnumsConsumer {
 
 	public static void main(String[] args) {
-//		KafkaConsumer<String,Customer> consumer = new KafkaConsumer<>(consumerProperties());
-//		consumer.subscribe(Collections.singleton(TOPIC_CUSTOMER));
-//
-//		System.out.println("Waiting for data");
-//
-//		while (true) {
-//			ConsumerRecords<String,Customer> records = consumer.poll(Duration.ofMillis(500));
-//			for (ConsumerRecord<String,Customer> record : records) {
-//				Customer customer = record.value();
-//				System.out.println(customer);
-//			}
-//			consumer.commitSync();
-//		}
+		KafkaConsumer<String,EnumWrapper> consumer = new KafkaConsumer<>(consumerProperties());
+		consumer.subscribe(singleton(TOPIC_ENUMS));
 
-		//		consumer.close();
+		System.out.println("Waiting for data");
+
+		while (true) {
+			ConsumerRecords<String,EnumWrapper> records = consumer.poll(Duration.ofMillis(500));
+			for (ConsumerRecord<String,EnumWrapper> record : records) {
+				EnumWrapper enumWrapper = record.value();
+				System.out.println(enumWrapper);
+			}
+//			consumer.commitSync();
+		}
+
 	}
 }
