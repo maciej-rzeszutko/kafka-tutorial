0bba831a2a241b24756aed144d8474da06a0005d
mackoko <maciej.rzeszutko@collibra.com>
2019-05-31 12:31:50 +0200

    Enum Wrapper

---
+++ avro_kafka_v2/src/main/java/com/github/mackoko/avro/enums/EnumsProducer.java
@@ -0,0 +1,26 @@
+package com.github.mackoko.avro.enums;
+
+
+import static com.github.mackoko.avro.util.KafkaUtil.TOPIC_ENUMS;
+import static com.github.mackoko.avro.util.KafkaUtil.producerProperties;
+import static com.github.mackoko.avro.util.KafkaUtil.sendAndClose;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class EnumsProducer {
+	public static void main(String[] args) {
+		sendAndClose(
+				new KafkaProducer<>(producerProperties()),
+				new ProducerRecord<>(
+						TOPIC_ENUMS,
+						EnumWrapper.newBuilder()
+								.setEnumWillBeAdded(EnumWillBeAdded.TWO)
+								.setEnumWillBeRemoved(EnumWillBeRemoved.TWO)
+								.setEnumWillBeRenamed(EnumWillBeRenamed.TWO)
+								.setEnumWillSwitchOrder(EnumWillSwitchOrder.TWO)
+						.build()
+				)
+		);
+	}
+}
