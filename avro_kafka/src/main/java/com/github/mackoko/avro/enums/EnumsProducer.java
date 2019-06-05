72be79da866c25fdb1927f60b394c19688ebe730
mackoko <maciej.rzeszutko@collibra.com>
2019-06-05 10:17:36 +0200

    control center with landoop ui for schemas

--- avro_kafka/src/main/java/com/github/mackoko/avro/enums/EnumsProducer.java
+++ avro_kafka/src/main/java/com/github/mackoko/avro/enums/EnumsProducer.java
@@ -5,22 +5,30 @@ import static com.github.mackoko.avro.util.KafkaUtil.TOPIC_ENUMS;
 import static com.github.mackoko.avro.util.KafkaUtil.producerProperties;
 import static com.github.mackoko.avro.util.KafkaUtil.sendAndClose;
 
+import java.util.Random;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class EnumsProducer {
 	public static void main(String[] args) {
-		sendAndClose(
-				new KafkaProducer<>(producerProperties()),
-				new ProducerRecord<>(
-						TOPIC_ENUMS,
-						EnumWrapper.newBuilder()
-								.setEnumWillBeAdded(EnumWillBeAdded.ONE)
-								.setEnumWillBeRemoved(EnumWillBeRemoved.ONE)
-								.setEnumWillBeRenamed(EnumWillBeRenamed.ONE)
-								.setEnumWillSwitchOrder(EnumWillSwitchOrder.ONE)
-						.build()
-				)
-		);
+
+		Random rand = new Random();
+		for (int i = 0; i < 1000 ; i++) {
+			sendAndClose(
+					new KafkaProducer<>(producerProperties()),
+					new ProducerRecord<>(
+							TOPIC_ENUMS,
+							Integer.toString(rand.nextInt(10)),
+							EnumWrapper.newBuilder()
+									.setEnumWillBeAdded(EnumWillBeAdded.ONE)
+									.setEnumWillBeRemoved(EnumWillBeRemoved.ONE)
+									.setEnumWillBeRenamed(EnumWillBeRenamed.ONE)
+									.setEnumWillSwitchOrder(EnumWillSwitchOrder.ONE)
+									.build()
+					)
+			);
+		}
+
 	}
 }
